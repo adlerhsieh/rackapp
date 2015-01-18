f849e64da8254609e25618ccd129a4491fb23b31
Adler <nkj20932@hotmail.com>
2015-01-18 12:28:21 +0800

    add default layout before rendering actual content

--- test-rack/framework.rb
+++ test-rack/framework.rb
@@ -10,11 +10,13 @@ class Framework
 			@@request
 		end
 
-
 		def render view_path, locals_hash={}
 			template_path = File.join("views", "#{view_path}.erb")
-			# view_buffer = Tilt::ERBTemplate.new('views/layout.erb').render(self, locals_hash)
-			Tilt::ERBTemplate.new(template_path).render(self, locals_hash)
+			view_buffer = Tilt::ERBTemplate.new(template_path).render(self, locals_hash)
+			Tilt::ERBTemplate.new('views/layout.erb').render(self, locals_hash) do
+				view_buffer
+			end
+			# Tilt::ERBTemplate.new(template_path).render(self, locals_hash)
 		end
 
 		def get(path, &block)
