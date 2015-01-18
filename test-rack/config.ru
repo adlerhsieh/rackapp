6a981df920905aa8580e68f02fd91c80db01b2b5
Adler <nkj20932@hotmail.com>
2015-01-18 11:46:20 +0800

    routing using regex

--- test-rack/config.ru
+++ test-rack/config.ru
@@ -4,12 +4,18 @@ class MyApp < Framework
 
 	# get 是class method，必須使用class variable才能使用
 	# run 的時候，以下部分都會先執行，才去跑call
-	get '/' do
-		"in root"
-	end
 
 	get '/hello' do
 		"hello"
+	end
+
+	get /\/books\/(\d+?)$/ do |id|
+		"This is book #{id}"
+	end
+
+	# 是否定義routing 有前後順序問題？
+	get '/' do
+		"in root"
 	end
 end
 
--- test-rack/framework.rb
+++ test-rack/framework.rb
@@ -4,16 +4,25 @@ class Framework
 	class << self
 		def get(path, &block)
 			# 定義get時，@@routes已經定義完成，可以依照request來回傳
-			@@routes[path.to_s] = block		
+			@@routes[path] = block		
 		end
 
 		def find_route_execute env
 			# 查詢env裡面的"PATH_INFO"這個key裡面的value
 			path = env['PATH_INFO']
+
 			# @@routes已經在get裡面被塞進去過，所以可以直接查詢
-			if @@routes.has_key? path
-				@@routes[path].call
+			# if @@routes.has_key? path
+			# 	@@routes[path].call
+			# end
+
+			@@routes.each do |path_exp, code_block|
+				var_arr = path.scan(path_exp).flatten
+				unless var_arr.empty?
+					return code_block.call(*var_arr)
+				end
 			end
+			""
 		end
 
 		# rack最先執行的method
