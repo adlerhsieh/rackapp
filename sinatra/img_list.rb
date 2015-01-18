bc0923efb336fda783d7e7254fc6581981c0e40b
Adler <nkj20932@hotmail.com>
2015-01-18 14:54:21 +0800

    add single page content and routing

--- sinatra/img_list.rb
+++ sinatra/img_list.rb
@@ -1,11 +1,21 @@
 class ImgList < Sinatra::Base
 	Images = [
-		{url: 'http://i.imgur.com/pE41fjj.jpg', title: "duolingo"},
-		{url: 'http://i.imgur.com/RuAHOIK.jpg', title: "McDonaled's Trip"},
-		{url: 'http://i.imgur.com/tWPf0go.jpg', title: "I think my Chinese food is undercooked"}
+		{id: 1, url: 'http://i.imgur.com/pE41fjj.jpg', title: "duolingo"},
+		{id: 2, url: 'http://i.imgur.com/RuAHOIK.jpg', title: "McDonaled's Trip"},
+		{id: 3, url: 'http://i.imgur.com/tWPf0go.jpg', title: "I think my Chinese food is undercooked"}
 	]
 
 	get '/imgs' do
 		erb :"index.html"
+	end
+
+	get '/imgs/:id' do
+		Images.each do |img|
+			if img[:id] == params[:id].to_i
+				@img = img
+				break
+			end
+		end 
+		erb :"show.html"
 	end
 end
